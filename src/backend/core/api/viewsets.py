dd3415610683c91199861e1c77b0193c0e7d1d32
Séverine Candelier <severine.candelier@icm-institute.org>
2025-06-03 12:08:23 +0200

    [backend] Apply diffs to the document model

--- src/backend/core/api/viewsets.py
+++ src/backend/core/api/viewsets.py
@@ -1,4 +1,5 @@
 """API endpoints"""
+import http
 # pylint: disable=too-many-lines
 
 import base64
@@ -639,6 +640,58 @@ class DocumentViewSet(
     def perform_destroy(self, instance):
         """Override to implement a soft delete instead of dumping the record in database."""
         instance.soft_delete()
+
+    def update(self, request, *args, **kwargs):
+        """This method will use the "content" field as a diff to update the document.
+        No revision for now, we assume a unique user is editing the document at a time."""
+        logger = logging.getLogger(__name__)
+        logger.info(f"Updating document with ID: {kwargs.get("pk")}")
+        logger.info(f"HTTP Method: {request.method}")
+
+        if request.method == http.HTTPMethod.PATCH:
+            # Handle partial updates (PATCH)
+            serializer = self.get_serializer(
+                data=request.data, partial=True, context=self.get_serializer_context()
+            )
+            serializer.is_valid(raise_exception=True)
+            logger.info(f"Received PATCH data: {serializer.validated_data}")
+
+            diffs = serializer.validated_data.get("content", "")
+            if diffs:
+                # Update the document content
+                document = self.get_object()
+                content = document.content
+                logger.info(f"Content before update: {content}")
+                # Create an Ironcalc model, apply diffs, and pass the updated content to the original update method
+                model = ic.create_user_model_from_bytes(base64.b64decode(content)) if content is not None \
+                    else ic.create_user_model("model", "en", "UTC")
+                logger.info(f"The model before applying diffs: {model}")
+                logger.info(f"Diffs: {diffs}")
+                model.apply_external_diffs(base64.b64decode(diffs))
+                updated_content = model.to_bytes()
+                logger.info(f"Updated content bytes: {updated_content}")
+                base64_content = base64.b64encode(updated_content).decode("utf-8")
+                logger.info(f"Updated content b64: {base64_content}")
+
+                # Prepare new data with updated content
+                data = request.data.copy()
+                data["content"] = base64_content
+                serializer = self.get_serializer(
+                    document,
+                    data=data,
+                    partial=True,
+                    context=self.get_serializer_context()
+                )
+                logger.info(f"Serializer data: {serializer.initial_data}")
+                serializer.is_valid(raise_exception=True)
+                logger.info(f"Valid data after applying diffs: {serializer.validated_data}")
+                self.perform_update(serializer)
+                logger.info("Document updated successfully.")
+                logger.info(f"Updated doc content: {document.content}")
+                return drf.response.Response(serializer.data)
+
+        return super().update(request, *args, **kwargs)
+
 
     @drf.decorators.action(
         detail=False,
